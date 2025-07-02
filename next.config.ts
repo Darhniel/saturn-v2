810d2b14ef66d460cfc2be2420cdbdaf9f67419b
Darhniel <81332139+Darhniel@users.noreply.github.com>
2025-07-02 17:35:35 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  matcher: ['/dashboard/:path*'],
+ /* matcher: ['/dashboard/:path*'],*/
 };
 
 export default nextConfig;
