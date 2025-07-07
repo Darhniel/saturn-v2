275a79193315c8eda447d3821c6e28428c376347
Darhniel <81332139+Darhniel@users.noreply.github.com>
2025-07-07 20:36:52 +0100

    update page.tsx

--- app/bitcoin-fund/page.tsx
+++ app/bitcoin-fund/page.tsx
@@ -75,7 +75,7 @@ export default function page() {
                                         </p>
                                         <Link
                                             href={"/login"}
-                                            className='text-white py-4 px-6 rounded-xl bg-[#1639CE]'
+                                            className='text-white py-4 px-6 rounded-xl bg-[#1639CE] block w-fit'
                                         >
                                             Invest Now
                                         </Link>
@@ -98,7 +98,8 @@ export default function page() {
                                         </p>
                                         <Link
                                             href={"/login"}
-                                            className='text-white py-4 px-6 rounded-xl bg-[#1639CE]'>
+                                            className='text-white py-4 px-6 rounded-xl bg-[#1639CE] block w-fit'
+                                        >
                                             Invest Now
                                         </Link>
                                     </div>
@@ -122,12 +123,13 @@ export default function page() {
                                         </p>
                                         <Link
                                             href={"/login"}
-                                            className='text-white py-4 px-6 rounded-xl bg-[#1639CE]'>
+                                            className='text-white py-4 px-6 rounded-xl bg-[#1639CE] block w-fit'
+                                        >
                                             Invest Now
                                         </Link>
                                     </div>
                                 </div>
-                                <div className='border rounded-3xl border-[#1639CE1A] lg:w-[59%] mix-blend-multiply relative bg-[#000D45] py-9 px-12'>
+                                <div className='border rounded-3xl border-[#1639CE1A] lg:w-[59%] mix-blend-multiply relative bg-[#000D45] p-9'>
                                     <div className='absolute inset-0 rounded-3xl'>
                                         <Image
                                             src={"/background.jpg"}
@@ -145,7 +147,11 @@ export default function page() {
                                         <p className="text-2xl mb-8 text-center">
                                             Gain access to real-time insights, detailed reports, and expert advice to help you stay ahead in the market and make confident investment decisions.
                                         </p>
-                                        <a href="/brochure/The Bitcoin Fund.pdf" className='text-white py-4 px-6 rounded-xl bg-[#1639CE] flex gap-3 items-center mx-auto' download>
+                                        <a 
+                                            href='/brochure/The Bitcoin Fund.pdf' 
+                                            className='text-white py-4 px-6 rounded-xl bg-[#1639CE] flex gap-3 items-center mx-auto w-fit'
+                                            download
+                                        >
                                             Download Brochure
                                             <DocumentArrowDownIcon
                                                 width={24}
